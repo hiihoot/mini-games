3816cd260bb105194ab7a4073acc28e239ce3876
hihoot <hihoot@proton.me>
2025-08-19 20:01:20 +0100

    refactored

--- tic tac toe/main.lua
+++ tic tac toe/main.lua
@@ -1,77 +1,48 @@
 
+--[[
+
+    ###Tic Tac Teo###
+    # Note:
+    This is the first Game :) I Made from a challenge of 49 mini games 
+    I learnd a lot (disipline ajmi), good luck for me on the next one
+    ps: ( it lucks ugly, probably i will get back polishing all of them one day who knows)
+
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    ## Next Game is: Memory Puzzle
+    --hihoot
+]]--
+
+local winCells
+local turn
+local tie
+local resetTimer
+
+
 local grid = {
     {"", "", ""},
     {"", "", ""},
     {"", "", ""}
 }
-local winCells = {}
-local turn = "X" 
-local tie = false
-local timer = 2
 
-function drawGrid()
-    for y = 1, #grid do
-        for x = 1, #grid[y] do
-                love.graphics.rectangle("line",
-                (x - 1) * 200,
-                (y - 1) * 200, 
-                200, 
-                200)
-        end
-    end 
-
-    for y = 1, #grid do
-        for x = 1, #grid[y] do
-            if grid[y][x] ~= "" then
-                local isWin = false
-
-                for _, cell in ipairs(winCells) do
-                    if cell.px == x and cell.py == y then
-                        isWin = true
-                        break
-                    end
-                end
-
-                if isWin then
-                    love.graphics.setColor(0, 1, 0, 0.6) -- green
-                    love.graphics.rectangle("fill",
-                        (x - 1) * 200,
-                        (y - 1) * 200,
-                        200, 200)
-                end
-
-                love.graphics.setColor(1, 1, 1)
-                love.graphics.printf(grid[y][x],
-                (x - 1) * 200,
-                (y - 1) * 200, 
-                200, 
-                "center")
-            end
-        end
-    end
- end
-
-function love.mousepressed(x, y, button, istouch, presses)
-    if button == 1 then
-        if #winCells <= 0 then
-            local col = math.floor(y / 200) + 1
-            local row = math.floor(x / 200) + 1
-        -- draw on empty cells
-            if grid[col][row] == "" then
-                grid[col][row] = turn
-            -- switch up 
-                if turn == "X" then
-                    turn = "O"
-                else
-                    turn = "X"
-                end
-            end
-        end
-        checkWin()
-    end
+local function resetGame() 
+    grid = {
+        {"", "", ""},
+        {"", "", ""},
+        {"", "", ""}
+    }
+    winCells = {}
+    tie = false
+    turn = "X"
+    resetTimer = nil
 end
 
-function checkWin()
+local function switchTurn()
+    turn = (turn == "X") and "O" or "X"
+end
+
+function checkGameState()
+    winCells = {}
+
     -- check rows
     for y = 1, #grid do
         if grid[y][1] ~= "" and grid[y][1] == grid[y][2] and grid[y][1] == grid[y][3] then
@@ -107,45 +78,88 @@ function checkWin()
         return true
     end
 
-    return nil
-end
-
-function reSet(dt)
     for y = 1, 3 do
         for x = 1, 3 do
-            if grid[y][x] ~= "" then
-                tie = true
-            else
-                tie = false
+            if grid[y][x] == "" then
+                return nil
             end
         end
     end
-
-    if tie or checkWin() then
-        timer = timer - dt
-        if timer <= 0 then 
-            grid = {
-                {"", "", ""},
-                {"", "", ""},
-                {"", "", ""}
-            }
-            winCells = {}
-            timer = 2
-            tie = false
-        end
-    end
+    tie = true
+    return "tie"
 end
 
+function love.mousepressed(x, y, button, istouch, presses)
+    if button ~= 1 or winCells[1] then return end
+
+        local col = math.floor(y / 200) + 1
+        local row = math.floor(x / 200) + 1
+        -- draw on empty cells
+        if grid[col][row] == "" then
+            grid[col][row] = turn
+            local state = checkGameState()
+
+            if not state then
+                switchTurn()
+            else
+                resetTimer = 2
+            end
+        end
+end
+
+function drawGrid()
+    love.graphics.setLineWidth(10)
+
+    for y = 1, #grid do
+        for x = 1, #grid[y] do
+                love.graphics.rectangle("line",
+                (x - 1) * 200,
+                (y - 1) * 200, 
+                200, 
+                200)
+        end
+    end 
+
+    for y = 1, #grid do
+        for x = 1, #grid[y] do
+            if grid[y][x] ~= "" then
+                local isWin = false
+
+                for _, cell in ipairs(winCells) do
+                    if cell.px == x and cell.py == y then isWin = true end
+                end
+
+                if isWin then
+                    love.graphics.setColor(0, 1, 0, 0.6) -- green
+                    love.graphics.rectangle("fill",
+                        (x - 1) * 200,
+                        (y - 1) * 200,
+                        200, 200)
+                end
+
+                love.graphics.setColor(1, 1, 1)
+                love.graphics.printf(grid[y][x],
+                (x - 1) * 200,
+                (y - 1) * 200, 
+                200, 
+                "center")
+            end
+        end
+    end
+ end
 
 function love.load()
     love.graphics.setFont(love.graphics.newFont(200))
+    resetGame()
 end
 
 function love.update(dt)
-        reSet(dt)
+    if resetTimer then
+        resetTimer = resetTimer - dt
+        if resetTimer <= 0 then resetGame() end
+    end
 end
 
 function love.draw()
-    love.graphics.setLineWidth(10)
     drawGrid()
 end
